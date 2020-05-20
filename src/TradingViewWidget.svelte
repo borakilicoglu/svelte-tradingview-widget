5adb4a198b9acf6de498f1ed8ba5e039cd18bb2d
Bora Kilicoglu <sbkilicoglu@gmail.com>
2020-05-20 10:54:46 +0300

    Updated README.MD

--- src/TradingViewWidget.svelte
+++ src/TradingViewWidget.svelte
@@ -13,60 +13,30 @@
   });
 
   function initWidget() {
-    if (typeof TradingView === "undefined") {
-      return;
+    if (typeof TradingView !== "undefined") {
+      new window.TradingView.widget(
+        Object.assign({ container_id: CONTAINER_ID }, options)
+      );
     }
-    new window.TradingView.widget(
-      Object.assign({ container_id: CONTAINER_ID }, options)
-    );
   }
 
   function appendScript(onload) {
-    if (!canUseDOM()) {
-      onload();
-      return;
+    if (document.getElementById(SCRIPT_ID) === null) {
+      const script = document.createElement("script");
+      script.id = SCRIPT_ID;
+      script.type = "text/javascript";
+      script.async = true;
+      script.src = "https://s3.tradingview.com/tv.js";
+      script.onload = onload;
+      document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+      const script = document.getElementById(SCRIPT_ID);
+      const oldOnload = script.onload;
+      return (script.onload = () => {
+        oldOnload();
+        onload();
+      });
     }
-
-    if (scriptExists()) {
-      if (typeof TradingView === "undefined") {
-        updateOnloadListener(onload);
-        return;
-      }
-      onload();
-      return;
-    }
-    const script = document.createElement("script");
-    script.id = SCRIPT_ID;
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = "https://s3.tradingview.com/tv.js";
-    script.onload = onload;
-    document.getElementsByTagName("head")[0].appendChild(script);
-  }
-
-  function canUseDOM() {
-    return (
-      typeof window !== "undefined" &&
-      window.document &&
-      window.document.createElement
-    );
-  }
-
-  function scriptExists() {
-    return getScriptElement() !== null;
-  }
-
-  function getScriptElement() {
-    return document.getElementById(SCRIPT_ID);
-  }
-
-  function updateOnloadListener(onload) {
-    const script = getScriptElement();
-    const oldOnload = script.onload;
-    return (script.onload = () => {
-      oldOnload();
-      onload();
-    });
   }
 </script>
 
